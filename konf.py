222cb7f584e0c0c5b1b4a22a34285ab7e5ac3b6b
maxkazyba <114348104+maxkazyba@users.noreply.github.com>
2025-10-09 12:00:16 +0300

    qq

--- konf.py
+++ konf.py
@@ -1,6 +1,22 @@
+import os
+
+def exp(a:str):
+    for k in os.environ:
+        a=a.replace("$"+k,os.environ[k])
+
 while True:
     a=input("VFS ")
+    b=a.split()
     if a == "exit":
         break
+    elif len(a)==0:
+        continue
+    elif b[0]=='ls':
+        print('ls')
+    elif b[0]=="cd":
+        print('cd')
+    elif b[0]=="echo":
+        a=exp(a)
+        print(a)
     else:
         print('CommandNotFoundException')
